0175fc8e9046c44b5bdd683dbf3c961b2a1b266e
hderwyn <hderwynbdo@gmail.com>
2024-04-23 02:04:17 -0400

    changing port .env

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const server = require("./src/app.js");
 const {conn} = require("./src/db.js");
+const {
+  DB_PORT
+} = process.env;
 
 conn.sync().then(() => {
   server.listen(3001, () => {
-    console.log("3001");
+    console.log("3001", DB_PORT);
   });
 });

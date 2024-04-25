d4e2ff4fc9cbc86cf436b89405940c655d683202
hderwyn <hderwynbdo@gmail.com>
2024-04-25 18:41:30 -0400

    editing CORS

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://henry-music.vercel.app, https://henry-musicapp-git-developer-musics-projects-c313a177.vercel.app, https://henry-musicapp-musics-projects-c313a177.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');

bc8a31f9f34d17ed390a89acad138f6a03bbe7e3
alan damian Fernandez <fernandeezalan20@gmail.com>
2024-04-28 20:18:56 -0300

    crear usuario con provedor

--- src/controllers/Users/postUsers.js
+++ src/controllers/Users/postUsers.js
@@ -2,7 +2,28 @@ const { Users } = require('../../db');
 
 const postUsers = async (req, res) => {
     try {
-        const { name, image, email, password } = req.body;
+        const { name, image, email, password, provider } = req.body;
+        console.log(req.body);
+
+        if (!name && !email) {
+            return res.status(400).json({ error: 'Faltan datos obligatorios' });
+        }
+
+        // Verificar si el usuario ya existe
+        const existingUser = await Users.findOne({ where: { email } });
+
+        if (existingUser && !provider) {
+            console.log('El usuario ya existe');
+            return res.status(400).json({ error: 'El usuario ya existe' });
+        }
+
+        if(provider === 'google' && existingUser){
+            return res.status(201).json({
+                message: 'secion iniciada exitosamente',
+            
+            });
+        }
+
         const newUser = await Users.create({
             name, 
             image,
@@ -10,10 +31,18 @@ const postUsers = async (req, res) => {
             password
         });
 
+        if(provider === 'google'){
+            await newUser.update({ esta_verificado: true });
+            return res.status(201).json({
+                message: 'Usuario creado exitosamente',
+            });
+        }
+
         return res.status(201).json(newUser);
+
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Error al crear un nuevo usuario' });
+        res.status(500).json({ error: 'Error al crear un nuevo usuario', message: error.message });
     }
 };
 

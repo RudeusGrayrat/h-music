86db13e164848244beb5f931a3ae1be60acd861c
hderwyn <hderwynbdo@gmail.com>
2024-04-28 12:16:01 -0400

    in this branch i'm trying to implement bcrypt to hash password, and jwt for verification of token in restricted areas of app

--- src/controllers/Users/postUsers.js
+++ src/controllers/Users/postUsers.js
@@ -1,16 +1,21 @@
 const { Users } = require('../../db');
+const { hashPassword } = require('../../utils/bcrypt');
 
 const postUsers = async (req, res) => {
     try {
         const { name, image, email, password } = req.body;
+        
+          const hashedPassword = await hashPassword(password);
+
         const newUser = await Users.create({
             name, 
             image,
             email, 
-            password
+            hashedPassword
         });
 
         return res.status(201).json(newUser);
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error al crear un nuevo usuario' });

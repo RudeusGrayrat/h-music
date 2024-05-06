d1db5f1c1eda3aa6101124b1fd23d5ea8ddecd0f
alan damian Fernandez <fernandeezalan20@gmail.com>
2024-05-06 07:54:52 -0300

    deletePlaylist

--- src/controllers/Playlists/deletePlaylist.js
+++ src/controllers/Playlists/deletePlaylist.js
@@ -2,19 +2,23 @@ const { Playlists } = require('../../db');
 
 //este controlador es para eliminar una playlist, toma el id de la playlist a eliminar por params y la 
 //elimina.
+const sendErrorResponse = (res, status, message) => {
+    console.error(message);
+    return res.status(status).json({ error: message });
+}
+
 const deletePlaylist = async (req, res) => {
-    
     const { id } = req.params;
 
-    try {
-        const playListToDelete = await Playlists.findOne({
-            where: {
-                id
-            }
-        });
-        if (!playListToDelete) {    
+    if (!id) {
+        return sendErrorResponse(res, 400, 'Falta el ID de la playlist en la solicitud');
+    }
 
-            return res.status(404).json({ error: 'La playlist no existe' });
+    try {
+        const playListToDelete = await Playlists.findOne({ where: { id } });
+
+        if (!playListToDelete) {    
+            return sendErrorResponse(res, 404, 'La playlist no existe');
         }
 
         await playListToDelete.destroy();
@@ -22,10 +26,15 @@ const deletePlaylist = async (req, res) => {
         return res.status(200).json({ message: 'La playlist se elimino correctamente' });
         
     } catch (error) {
-
-        return res.status(500).json({ error: error.message });
+        // Manejar diferentes tipos de errores de manera más específica
+        if (error instanceof ValidationError) {
+            return sendErrorResponse(res, 400, error.message);
+        } else if (error instanceof ForeignKeyConstraintError) {
+            return sendErrorResponse(res, 409, 'La playlist no puede ser eliminada porque está siendo referenciada por otras entidades');
+        } else {
+            return sendErrorResponse(res, 500, error.message);
+        }
     }
-
 }
 
-module.exports = deletePlaylist; 
+module.exports = deletePlaylist;

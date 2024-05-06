0f8d8cd7a935209f27a9a340092a84d9d901e9ea
hderwyn <hderwynbdo@gmail.com>
2024-05-05 20:02:25 -0400

    fixeando controlador de deleteSongInPlaylist.js

--- src/controllers/Playlists/deleteSongInPlaylist.js
+++ src/controllers/Playlists/deleteSongInPlaylist.js
@@ -3,22 +3,20 @@ const { PlaylistDetails } = require('../../db');
 //este controlador es para eliminar una cancion de una playlist, recibe el id de la cancion y 
 //el id de la playlist por params.
 const deleteSongInPlaylist = async (req, res) => {
-  const { songId, playlistId } = req.params;
+  const { id } = req.query;
 
   try {
     const songToDelete = await PlaylistDetails.findOne({
       where: {
-        SongsID: songId,
-        PlaylistID: playlistId,
+        id: id,
       },
     });
 
     if (!songToDelete) {
-      return res.status(404).json({ message: 'La canción no se encuentra en la playlist' });
+      return res.status(404).json({ message: 'La canción no se encuentra en ninguna playlist' });
     }
 
-    
-    await songToDelete.destroy();
+     await songToDelete.destroy();
 
     return res.status(200).json({ message: 'La canción se eliminó correctamente de la playlist' });
 

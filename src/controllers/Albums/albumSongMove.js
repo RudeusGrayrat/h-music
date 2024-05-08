a4c49211dbfbae9a4df0ee511f7fdf2ea790b4ec
RudeusGrayrat <miguelnc2502@gmail.com>
2024-05-07 20:10:48 -0500

    eliminar cancion de album

--- src/controllers/Albums/albumSongMove.js
+++ src/controllers/Albums/albumSongMove.js
@@ -1,32 +1,23 @@
 const { Albums, Songs } = require("../../db");
 
 const addSongsToAlbum = async (req, res) => {
-    //este controlador a pesar de que dice addSongs es un put, ya que edita el modelo de la canción para cambiarle
-    //el id del album, ya que es por donde se referencia, en este caso, la cancion solo puede tener un album.
+    const {songsiD } = req.query;
 
-    const { albumsId, songsiD } = req.query;
     try {
-
-        if (!albumsId || !songsiD) return res.status(400).json({ error: "Faltan datos" });
-
+        if (!songsiD) return res.status(400).json({ error: "Faltan datos" });
         const album = await Albums.findByPk(albumsId);
-
         if (!album) return res.status(404).json({ error: "Album no encontrado" });
-        
         const song = await Songs.findOne({
             where: {
                 id: songsiD
             }
         });
         if (!song) return res.status(404).json({ error: "Cancion no encontrada" });
+        await song.update({ AlbumsID: 1 });
 
-        await song.update({ AlbumsID: albumsId });
-        
-        res.status(200).json({message: "Cancion agregada al album"});
+        return res.status(200).json({ message: "Cancion agregada al album" });
 
-        
     } catch (error) {
-
         res.status(400).json({ error: error.message });
     }
 }

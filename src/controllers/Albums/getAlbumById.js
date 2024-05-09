dae37c41bd6084f511dc2f448b290ef7cc47d800
iska1234 <sadhuisaac1@gmail.com>
2024-05-09 17:08:01 -0500

    Fix commit 2

--- src/controllers/Albums/getAlbumById.js
+++ src/controllers/Albums/getAlbumById.js
@@ -4,23 +4,30 @@ const getAlbumById = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const album = await Albums.findByPk(id, {
-            include: {
-                model: Songs,
-                where: { AlbumsID: id }
-            }
+        const album = await Albums.findOne({ 
+            where: { id },
+            include: [{ model: Songs }] 
         });
 
         if (!album) {
-            return res.status(404).json({ message: 'No se encontró ningún álbum con el ID proporcionado' });
+            const mensaje = "No se encontró ningún álbum con el ID proporcionado";
+            return res.status(404).json({ mensaje });
+        } else {
+
+            const respuesta = {
+                album: {
+                    id: album.id,
+                    name: album.name,
+                    image: album.image,
+                    Songs: album.Songs || []
+                }
+            };
+            return res.status(200).json(respuesta);
         }
 
-        const songs = album.Songs ? album.Songs : [];
-
-        return res.status(200).json({ ...album.toJSON(), songs });
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
-};
+}
 
 module.exports = getAlbumById;

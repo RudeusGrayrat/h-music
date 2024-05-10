16d5b97687338c676db9e77e87e657daed861d35
Derwyn Herrera <hderwynbdo@gmail.com>
2024-05-10 17:30:04 -0400

    push main

--- src/controllers/Verification/verification.js
+++ src/controllers/Verification/verification.js
@@ -44,8 +44,70 @@ const verification = async (req, res) => {
       from: 'hmusic.proyecto@mail.ee',
       to: user.email,
       subject: 'Henry Music - Verificación de correo electrónico',
-      text: `Por favor, verifica tu correo electrónico siguiendo el siguiente enlace: 
-      ${FRONTURL}/${url}/${userName}?id=${id}&token=${verificationToken}`,
+      html:`<!DOCTYPE html>
+      <html lang="es">
+      
+      <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <meta http-equiv="x-ua-compatible" content="ie=edge">
+      
+          <title>Henry Music Invoice</title>
+      
+      </head>
+      
+      <body style="letter-spacing: -0.3px;font-weight: 600;">
+          <div
+              style="width: 500px;margin: auto;border: 2px solid #000;padding: 20px;box-shadow: 0px 0px 10px rgba(0,0,0,0.1);background-color: #171717;border-radius: 15px;display: block;clear: both; hover: 0px 0px 10px rgba(0,0,0,0.2);">
+              <div style="display: block;clear: both;color: #ffffff; margin: 30px">
+                  <div>
+                      <div style="display: flex; justify-content: center;">
+                          <h1 style="background-color: rgb(255, 196, 0); padding: 20px; border-radius: 15px;color:#000">
+                              HENRY
+                              MUSIC</h1>
+                      </div>
+                      <div style="margin-top: 20px;">
+                          De
+                          <address>
+                              <strong>H MUSIC</strong>
+                              <br>
+                                  Email: hmusic.proyecto@mail.ee
+                          </address>
+                      </div>
+                      <div style="margin-top: 20px;">
+                          Para
+                          <address>
+                              <strong style="text-transform: uppercase;">${user.name}</strong><br>
+      
+                              Email: ${user.email}
+                          </address>
+                      </div>
+                  </div>
+                  <br><br>
+                  <div style="display: block;clear: both;">
+                      <span style="color: #ffffff;"> Muchas gracias por registrate en Henry Music!</span>
+                      <br><br>
+                      <span style="color: #ffffff;"> Para verificar email presione el boton!</span>
+                  </div>
+                  <br>
+                  <br>
+                  <br>
+                        <div style="display: flex; justify-content: center; ">
+                        <a href="${FRONTURL}/${url}/${userName}?id=${id}&token=${verificationToken}" style="text-decoration: none;">
+                          <button style="padding: 15px; background-color: #ffffff;border-radius: 10px;background-color: rgb(255, 196, 0); hover: 0px 0px 10px rgba(0,0,0,0.2);">
+                            <strong>H MUSIC</strong>
+                          </button>
+                        </a>
+                  </div>
+                  <br>
+              </div>
+          </div>
+      </body>
+      
+      </html>`
+      
+      //`Por favor, verifica tu correo electrónico siguiendo el siguiente enlace: 
+      //${FRONTURL}/${url}/${userName}?id=${id}&token=${verificationToken}`,
     });
 
     res.status(200).json(sendingEmail);

3afe1a6b5645ee6fc13a4d56be82851fd3c1bffd
Miguel23nc <1392023@senati.pe>
2025-03-30 21:13:29 -0500

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const server = require("./src/app.js");
-const {conn} = require("./src/db.js");
+const { conn } = require("./src/db.js");
 const {
   PORT
 } = process.env;
 
 conn.sync({ alter: true }).then(() => {
-  server.listen(PORT, "0.0.0.0", () => {
+  server.listen(PORT || 3001, "0.0.0.0", () => {
     console.log(`${PORT}`);
   });
 });
